bfe1b3d12dfb122469215852680870f7e3c465e8
Shivendra Singh Lodhi <112497430+tsrshiv@users.noreply.github.com>
2025-04-24 13:23:42 +0530

    Create 2103_A.cpp

---
+++ 2103_A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
+        set<int>st(arr.begin(), arr.end());
+        cout << st.size() << endl;
+    }
+    return 0;
+}
